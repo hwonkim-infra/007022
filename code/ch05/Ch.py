9ca40fc787a3e6611aa1e3e0405d7246b5bf7564
hwonkim-infra <52963259+hwonkim-infra@users.noreply.github.com>
2019-11-26 23:49:01 +0900

    Update Ch.py

--- code/ch05/Ch.py
+++ code/ch05/Ch.py
@@ -114,6 +114,694 @@ plt.tight_layout()
 plt.show()
 
 
+# In[11]:
+
+
+from sklearn.decomposition import PCA
+
+pca = PCA()
+X_train_pca = pca.fit_transform(X_train_std)
+pca.explained_variance_ratio_
+
+
+# In[12]:
+
+
+plt.bar(range(1, 14), pca.explained_variance_ratio_, alpha=0.5, align='center')
+plt.step(range(1, 14), np.cumsum(pca.explained_variance_ratio_), where='mid')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal components')
+
+plt.show()
+
+
+# In[13]:
+
+
+
+pca = PCA(n_components=2)
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+
+
+# In[14]:
+
+
+plt.scatter(X_train_pca[:, 0], X_train_pca[:, 1])
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.show()
+
+
+# In[15]:
+
+
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+    # 마커와 컬러맵을 준비합니다
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # 결정 경계를 그립니다
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # 클래스 샘플을 표시합니다
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.6, 
+                    c=cmap.colors[idx],
+                    edgecolor='black',
+                    marker=markers[idx], 
+                    label=cl)
+
+
+# In[16]:
+
+
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+
+pca = PCA(n_components=2)
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+
+lr = LogisticRegression(solver='liblinear', multi_class='auto')
+lr = lr.fit(X_train_pca, y_train)
+
+
+# In[17]:
+
+
+
+plot_decision_regions(X_train_pca, y_train, classifier=lr)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
+
+
+# In[18]:
+
+
+plot_decision_regions(X_test_pca, y_test, classifier=lr)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
+
+
+# In[19]:
+
+
+pca = PCA(n_components=None)
+X_train_pca = pca.fit_transform(X_train_std)
+pca.explained_variance_ratio_
+
+
+# In[20]:
+
+
+
+np.set_printoptions(precision=4)
+
+mean_vecs = []
+for label in range(1, 4):
+    mean_vecs.append(np.mean(X_train_std[y_train == label], axis=0))
+    print('MV %s: %s\n' % (label, mean_vecs[label - 1]))
+
+
+# In[21]:
+
+
+d = 13 # 특성의 수
+S_W = np.zeros((d, d))
+for label, mv in zip(range(1, 4), mean_vecs):
+    class_scatter = np.zeros((d, d))  # scatter matrix for each class
+    for row in X_train_std[y_train == label]:
+        row, mv = row.reshape(d, 1), mv.reshape(d, 1)  # make column vectors
+        class_scatter += (row - mv).dot((row - mv).T)
+    S_W += class_scatter                          # sum class scatter matrices
+
+print('클래스 내의 산포 행렬: %sx%s' % (S_W.shape[0], S_W.shape[1]))
+
+
+# In[22]:
+
+
+print('클래스 레이블 분포: %s' 
+      % np.bincount(y_train)[1:])
+
+
+# In[23]:
+
+
+d = 13  # 특성의 수
+S_W = np.zeros((d, d))
+for label, mv in zip(range(1, 4), mean_vecs):
+    class_scatter = np.cov(X_train_std[y_train == label].T, bias=True)
+    S_W += class_scatter
+print('스케일 조정된 클래스 내의 산포 행렬: %sx%s' % (S_W.shape[0],
+                                                     S_W.shape[1]))
+
+
+# In[24]:
+
+
+mean_overall = np.mean(X_train_std, axis=0)
+mean_overall = mean_overall.reshape(d, 1)  # 열 벡터로 만들기
+d = 13  # 특성의 수
+S_B = np.zeros((d, d))
+for i, mean_vec in enumerate(mean_vecs):
+    n = X_train[y_train == i + 1, :].shape[0]
+    mean_vec = mean_vec.reshape(d, 1)  # 열 벡터로 만들기
+    S_B += n * (mean_vec - mean_overall).dot((mean_vec - mean_overall).T)
+
+print('클래스 간의 산포 행렬: %sx%s' % (S_B.shape[0], S_B.shape[1]))
+
+
+# In[25]:
+
+
+eigen_vals, eigen_vecs = np.linalg.eig(np.linalg.inv(S_W).dot(S_B))
+
+
+# In[26]:
+
+
+# (고윳값, 고유벡터) 튜플의 리스트를 만듭니다.
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i])
+               for i in range(len(eigen_vals))]
+
+# (고윳값, 고유벡터) 튜플을 큰 값에서 작은 값 순서대로 정렬합니다.
+eigen_pairs = sorted(eigen_pairs, key=lambda k: k[0], reverse=True)
+
+# 고윳값의 역순으로 올바르게 정렬되었는지 확인합니다.
+
+print('내림차순의 고윳값:\n')
+for eigen_val in eigen_pairs:
+    print(eigen_val[0])
+
+
+# In[27]:
+
+
+tot = sum(eigen_vals.real)
+discr = [(i / tot) for i in sorted(eigen_vals.real, reverse=True)]
+cum_discr = np.cumsum(discr)
+
+plt.bar(range(1, 14), discr, alpha=0.5, align='center',
+        label='individual "discriminability"')
+plt.step(range(1, 14), cum_discr, where='mid',
+         label='cumulative "discriminability"')
+plt.ylabel('"discriminability" ratio')
+plt.xlabel('Linear Discriminants')
+plt.ylim([-0.1, 1.1])
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
+
+
+# In[28]:
+
+
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis].real,
+              eigen_pairs[1][1][:, np.newaxis].real))
+print('행렬 W:\n', w)
+
+
+# In[29]:
+
+
+
+X_train_lda = X_train_std.dot(w)
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    plt.scatter(X_train_lda[y_train == l, 0],
+                X_train_lda[y_train == l, 1] * (-1),
+                c=c, label=l, marker=m)
+
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
+plt.legend(loc='lower right')
+plt.tight_layout()
+plt.show()
+
+
+# In[30]:
+
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+
+lda = LDA(n_components=2)
+X_train_lda = lda.fit_transform(X_train_std, y_train)
+
+
+# In[31]:
+
+
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression(solver='liblinear', multi_class='auto')
+lr = lr.fit(X_train_lda, y_train)
+
+plot_decision_regions(X_train_lda, y_train, classifier=lr)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
+
+
+# In[32]:
+
+
+X_test_lda = lda.transform(X_test_std)
+
+plot_decision_regions(X_test_lda, y_test, classifier=lr)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
+
+
+# In[33]:
+
+
+y_uniq, y_count = np.unique(y_train, return_counts=True)
+priors = y_count / X_train_std.shape[0]
+priors
+
+
+# In[34]:
+
+
+s_w = np.zeros((X_train_std.shape[1], X_train_std.shape[1]))
+for i, label in enumerate(y_uniq):
+    # 1/n로 나눈 공분산 행렬을 얻기 위해 bias=True로 지정합니다.
+    s_w += priors[i] * np.cov(X_train_std[y_train == label].T, bias=True)
+
+
+# In[35]:
+
+
+
+s_b = np.zeros((X_train_std.shape[1], X_train_std.shape[1]))
+for i, mean_vec in enumerate(mean_vecs):
+    n = X_train_std[y_train == i + 1].shape[0]
+    mean_vec = mean_vec.reshape(-1, 1)
+    s_b += priors[i] * (mean_vec - mean_overall).dot((mean_vec - mean_overall).T)
+
+
+# In[36]:
+
+
+
+import scipy
+ei_val, ei_vec = scipy.linalg.eigh(s_b, s_w)
+ei_vec = ei_vec[:, np.argsort(ei_val)[::-1]]
+ei_vec /= np.linalg.norm(ei_vec, axis=0)
+
+
+# In[37]:
+
+
+lda_eigen = LDA(solver='eigen')
+lda_eigen.fit(X_train_std, y_train)
+
+
+# In[38]:
+
+
+# 클래스 내의 산포 행렬은 covariance_ 속성에 저장되어 있습니다.
+np.allclose(s_w, lda_eigen.covariance_)
+
+
+# In[39]:
+
+
+Sb = np.cov(X_train_std.T, bias=True) - lda_eigen.covariance_
+np.allclose(Sb, s_b)
+
+
+# In[40]:
+
+
+np.allclose(lda_eigen.scalings_[:, :2], ei_vec[:, :2])
+
+
+# In[41]:
+
+
+np.allclose(lda_eigen.transform(X_test_std), np.dot(X_test_std, ei_vec[:, :2]))
+
+
+# In[42]:
+
+
+from scipy.spatial.distance import pdist, squareform
+from scipy import exp
+from scipy.linalg import eigh
+import numpy as np
+
+def rbf_kernel_pca(X, gamma, n_components):
+    """
+    RBF 커널 PCA 구현
+
+    매개변수
+    ------------
+    X: {넘파이 ndarray}, shape = [n_samples, n_features]
+        
+    gamma: float
+      RBF 커널 튜닝 매개변수
+        
+    n_components: int
+      반환할 주성분 개수
+
+    반환값
+    ------------
+     X_pc: {넘파이 ndarray}, shape = [n_samples, k_features]
+       투영된 데이터셋
+
+    """
+    # MxN 차원의 데이터셋에서 샘플 간의 유클리디안 거리의 제곱을 계산합니다.
+    sq_dists = pdist(X, 'sqeuclidean')
+
+    # 샘플 간의 거리를 정방 대칭 행렬로 변환합니다.
+    mat_sq_dists = squareform(sq_dists)
+
+    # 커널 행렬을 계산합니다.
+    K = exp(-gamma * mat_sq_dists)
+
+    # 커널 행렬을 중앙에 맞춥니다.
+    N = K.shape[0]
+    one_n = np.ones((N, N)) / N
+    K = K - one_n.dot(K) - K.dot(one_n) + one_n.dot(K).dot(one_n)
+
+    # 중앙에 맞춰진 커널 행렬의 고윳값과 고유벡터를 구합니다.
+    # scipy.linalg.eigh 함수는 오름차순으로 반환합니다.
+    eigvals, eigvecs = eigh(K)
+    eigvals, eigvecs = eigvals[::-1], eigvecs[:, ::-1]
+
+    # 최상위 k 개의 고유벡터를 선택합니다(결과값은 투영된 샘플입니다).
+    X_pc = np.column_stack([eigvecs[:, i]
+                            for i in range(n_components)])
+
+    return X_pc
+
+
+# In[43]:
+
+
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_moons
+
+X, y = make_moons(n_samples=100, random_state=123)
+
+plt.scatter(X[y == 0, 0], X[y == 0, 1], color='red', marker='^', alpha=0.5)
+plt.scatter(X[y == 1, 0], X[y == 1, 1], color='blue', marker='o', alpha=0.5)
+
+plt.tight_layout()
+plt.show()
+
+
+# In[44]:
+
+
+from sklearn.decomposition import PCA
+
+scikit_pca = PCA(n_components=2)
+X_spca = scikit_pca.fit_transform(X)
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+
+ax[0].scatter(X_spca[y == 0, 0], X_spca[y == 0, 1],
+              color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y == 1, 0], X_spca[y == 1, 1],
+              color='blue', marker='o', alpha=0.5)
+
+ax[1].scatter(X_spca[y == 0, 0], np.zeros((50, 1)) + 0.02,
+              color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y == 1, 0], np.zeros((50, 1)) - 0.02,
+              color='blue', marker='o', alpha=0.5)
+
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+
+plt.tight_layout()
+plt.show()
+
+
+# In[45]:
+
+
+
+X_kpca = rbf_kernel_pca(X, gamma=15, n_components=2)
+
+fig, ax = plt.subplots(nrows=1,ncols=2, figsize=(7,3))
+ax[0].scatter(X_kpca[y==0, 0], X_kpca[y==0, 1], 
+            color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_kpca[y==1, 0], X_kpca[y==1, 1],
+            color='blue', marker='o', alpha=0.5)
+
+ax[1].scatter(X_kpca[y==0, 0], np.zeros((50,1))+0.02, 
+            color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_kpca[y==1, 0], np.zeros((50,1))-0.02,
+            color='blue', marker='o', alpha=0.5)
+
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+
+plt.tight_layout()
+plt.show()
+
+
+# In[46]:
+
+
+from sklearn.datasets import make_circles
+
+X, y = make_circles(n_samples=1000, random_state=123, noise=0.1, factor=0.2)
+
+plt.scatter(X[y == 0, 0], X[y == 0, 1], color='red', marker='^', alpha=0.5)
+plt.scatter(X[y == 1, 0], X[y == 1, 1], color='blue', marker='o', alpha=0.5)
+
+plt.tight_layout()
+plt.show()
+
+
+# In[47]:
+
+
+scikit_pca = PCA(n_components=2)
+X_spca = scikit_pca.fit_transform(X)
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+
+ax[0].scatter(X_spca[y == 0, 0], X_spca[y == 0, 1],
+              color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y == 1, 0], X_spca[y == 1, 1],
+              color='blue', marker='o', alpha=0.5)
+
+ax[1].scatter(X_spca[y == 0, 0], np.zeros((500, 1)) + 0.02,
+              color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y == 1, 0], np.zeros((500, 1)) - 0.02,
+              color='blue', marker='o', alpha=0.5)
+
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+
+plt.tight_layout()
+plt.show()
+
+
+# In[48]:
+
+
+X_kpca = rbf_kernel_pca(X, gamma=15, n_components=2)
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+ax[0].scatter(X_kpca[y == 0, 0], X_kpca[y == 0, 1],
+              color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_kpca[y == 1, 0], X_kpca[y == 1, 1],
+              color='blue', marker='o', alpha=0.5)
+
+ax[1].scatter(X_kpca[y == 0, 0], np.zeros((500, 1)) + 0.02,
+              color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_kpca[y == 1, 0], np.zeros((500, 1)) - 0.02,
+              color='blue', marker='o', alpha=0.5)
+
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+
+plt.tight_layout()
+plt.show()
+
+
+# In[49]:
+
+
+from scipy.spatial.distance import pdist, squareform
+from scipy import exp
+from scipy.linalg import eigh
+import numpy as np
+
+def rbf_kernel_pca(X, gamma, n_components):
+    """
+    RBF 커널 PCA 구현
+
+    매개변수
+    ------------
+    X: {넘파이 ndarray}, shape = [n_samples, n_features]
+        
+    gamma: float
+      RBF 커널 튜닝 매개변수
+        
+    n_components: int
+      반환할 주성분 개수
+
+    Returns
+    ------------
+     alphas: {넘파이 ndarray}, shape = [n_samples, k_features]
+       투영된 데이터셋
+     
+     lambdas: list
+       고윳값
+
+    """
+    # MxN 차원의 데이터셋에서 샘플 간의 유클리디안 거리의 제곱을 계산합니다.
+    sq_dists = pdist(X, 'sqeuclidean')
+
+    # 샘플 간의 거리를 정방 대칭 행렬로 변환합니다.
+    mat_sq_dists = squareform(sq_dists)
+
+    # 커널 행렬을 계산합니다.
+    K = exp(-gamma * mat_sq_dists)
+
+    # 커널 행렬을 중앙에 맞춥니다.
+    N = K.shape[0]
+    one_n = np.ones((N, N)) / N
+    K = K - one_n.dot(K) - K.dot(one_n) + one_n.dot(K).dot(one_n)
+
+    # 중앙에 맞춰진 커널 행렬의 고윳값과 고유 벡터를 구합니다.
+    # scipy.linalg.eigh 함수는 오름차순으로 반환합니다.
+    eigvals, eigvecs = eigh(K)
+    eigvals, eigvecs = eigvals[::-1], eigvecs[:, ::-1]
+
+    # 최상위 k 개의 고유 벡터를 선택합니다(투영 결과).
+    alphas = np.column_stack([eigvecs[:, i]
+                              for i in range(n_components)])
+
+    # 고유 벡터에 상응하는 고윳값을 선택합니다.
+    lambdas = [eigvals[i] for i in range(n_components)]
+
+    return alphas, lambdas
+
+
+# In[50]:
+
+
+
+X, y = make_moons(n_samples=100, random_state=123)
+alphas, lambdas = rbf_kernel_pca(X, gamma=15, n_components=1)
+
+
+# In[51]:
+
+
+x_new = X[25]
+x_new
+
+
+# In[52]:
+
+
+x_proj = alphas[25] # 원본 투영
+x_proj
+
+
+# In[53]:
+
+
+def project_x(x_new, X, gamma, alphas, lambdas):
+    pair_dist = np.array([np.sum((x_new - row)**2) for row in X])
+    k = np.exp(-gamma * pair_dist)
+    return k.dot(alphas / lambdas)
+
+# 새로운 데이터포인트를 투영합니다.
+x_reproj = project_x(x_new, X, gamma=15, alphas=alphas, lambdas=lambdas)
+x_reproj
+
+
+# In[54]:
+
+
+plt.scatter(alphas[y == 0, 0], np.zeros((50)),
+            color='red', marker='^', alpha=0.5)
+plt.scatter(alphas[y == 1, 0], np.zeros((50)),
+            color='blue', marker='o', alpha=0.5)
+plt.scatter(x_proj, 0, color='black',
+            label='original projection of point X[25]', marker='^', s=100)
+plt.scatter(x_reproj, 0, color='green',
+            label='remapped point X[25]', marker='x', s=500)
+plt.legend(scatterpoints=1)
+
+plt.tight_layout()
+plt.show()
+
+
+# 사이킷런의 커널 PCA
+
+from sklearn.decomposition import KernelPCA
+
+X, y = make_moons(n_samples=100, random_state=123)
+scikit_kpca = KernelPCA(n_components=2, kernel='rbf', gamma=15)
+X_skernpca = scikit_kpca.fit_transform(X)
+
+plt.scatter(X_skernpca[y == 0, 0], X_skernpca[y == 0, 1],
+            color='red', marker='^', alpha=0.5)
+plt.scatter(X_skernpca[y == 1, 0], X_skernpca[y == 1, 1],
+            color='blue', marker='o', alpha=0.5)
+
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+
 
 
 

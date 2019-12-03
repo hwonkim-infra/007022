0802bc9f69ef288358d2f7fe01e836e0b641fd03
hwonkim-infra <52963259+hwonkim-infra@users.noreply.github.com>
2019-12-03 23:40:57 +0900

    Update ch06.py

--- code/ch06/ch06.py
+++ code/ch06/ch06.py
@@ -1,5 +1,3 @@
-
-
 import pandas as pd
 
 #df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data', header=None)
@@ -48,40 +46,33 @@ print('테스트 정확도: %.3f' % pipe_lr.score(X_test, y_test))
 
 import numpy as np
 from sklearn.model_selection import StratifiedKFold
-    
+    # (특히 클래스 비율이 동등하지 않을 때) 더 나은 편향과 분산 추정.
 
-kfold = StratifiedKFold(n_splits=10,
-                        random_state=1).split(X_train, y_train)
-
+kfold = StratifiedKFold(n_splits=10, random_state=1).split(X_train, y_train)
+    # n_splits: 폴드 갯수 지정. 
 scores = []
-for k, (train, test) in enumerate(kfold):
-    pipe_lr.fit(X_train[train], y_train[train])
-    score = pipe_lr.score(X_train[test], y_train[test])
-    scores.append(score)
-    print('폴드: %2d, 클래스 분포: %s, 정확도: %.3f' % (k+1,
-          np.bincount(y_train[train]), score))
+for k, (train, test) in enumerate(kfold):                           # kfold 반복자를 사용하여 train 인덱스를 로지스틱 회귀 파이프라인 훈련에 사용
+    pipe_lr.fit(X_train[train], y_train[train])                         # pipe_lr 파이프라인으로 각 반복에서 샘플 스케일을 조절
+    score = pipe_lr.score(X_train[test], y_train[test])            # test index: 모델의 정확도 점수 계산.
+    scores.append(score)                                             # 이 점수를 리스트에 모아 추정 정확도의 평균과 표준편차 계산
+    print('폴드: %2d, 클래스 분포: %s, 정확도: %.3f' % (k+1, np.bincount(y_train[train]), score))
     
 print('\nCV 정확도: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
 
 
-# In[12]:
+# cross_val_score: 회귀에는 
 
 
 from sklearn.model_selection import cross_val_score
-
-scores = cross_val_score(estimator=pipe_lr,
-                         X=X_train,
-                         y=y_train,
-                         cv=10,
-                         n_jobs=1)
+    # 각 폴드의 평가를 복수 코어에 분산 연산 
+scores = cross_val_score(estimator=pipe_lr, X=X_train, y=y_train, cv=10, n_jobs=1)
 print('CV 정확도 점수: %s' % scores)
 print('CV 정확도: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
 
 
-# In[13]:
-
 
 from sklearn.model_selection import cross_validate
+    # 교차검증에 여러 측정 지표 사용할 수 있는 함수. 각 폴드에서 훈련 테스트 시간 반환. 
 
 scores = cross_validate(estimator=pipe_lr, X=X_train, y=y_train, scoring=['accuracy'], cv=10, n_jobs=-1, return_train_score=False)
 print('CV 정확도 점수: %s' % scores['test_accuracy'])
@@ -93,17 +84,21 @@ print('CV 정확도: %.3f +/- %.3f' % (np.mean(scores['test_accuracy']), np.std(
 
 import matplotlib.pyplot as plt
 from sklearn.model_selection import learning_curve
-
+    # 계층별 k겹 교차 검증 사용하여 검증 정확도 계산
+    
 pipe_lr = make_pipeline(StandardScaler(), LogisticRegression(solver='liblinear', penalty='l2', random_state=1))
 train_sizes, train_scores, test_scores = learning_curve(estimator=pipe_lr, X=X_train, y=y_train, train_sizes=np.linspace(0.1, 1.0, 10), cv=10, n_jobs=1)
+    # 일정 간격으로 훈련 세트 비율 10개 설정. cv = 10겹 교차 검증. 
 
 train_mean = np.mean(train_scores, axis=1)
 train_std = np.std(train_scores, axis=1)
 test_mean = np.mean(test_scores, axis=1)
 test_std = np.std(test_scores, axis=1)
-
+    # 반환된 훈련 / 교차검증 점수로부터 세트 크기별로 평균 정확도 계산하여 그래프
+    
 plt.plot(train_sizes, train_mean, color='blue', marker='o', markersize=5, label='training accuracy')
 plt.fill_between(train_sizes, train_mean + train_std, train_mean - train_std, alpha=0.15, color='blue')
+    # 그래프에 평균 정확도 표준편차 이용 추정 분산 표기
 plt.plot(train_sizes, test_mean, color='green', linestyle='--', marker='s', markersize=5, label='validation accuracy')
 plt.fill_between(train_sizes, test_mean + test_std, test_mean - test_std, alpha=0.15, color='green')
 plt.grid()
@@ -117,14 +112,16 @@ plt.show()
 
 # 검증 곡선으로 과대적합과 과소적합 조사
 
-
 from sklearn.model_selection import validation_curve
+    # 샘플 크기 함수 > 모델 파라미터 값 함수로 그림
     # validation_curve: 최적화할 파라미터 이름과 범위, 그리고 성능 기준을
     # param_name, param_range, scoring 인수로 받아 파라미터 범위 모든 경우에 대해 성능 기준 계산
 
 param_range = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
 train_scores, test_scores = validation_curve(estimator=pipe_lr, X=X_train, y=y_train, param_name='logisticregression__C', param_range=param_range, cv=10)
-
+    # LogisticRegression 분류기의 규제 매개변수 C 지정: C를 줄이면 규제 강도가 높아지고 과소적합. C를 늘리면 과대적합 양상
+    
+    
 train_mean = np.mean(train_scores, axis=1)
 train_std = np.std(train_scores, axis=1)
 test_mean = np.mean(test_scores, axis=1)
@@ -144,20 +141,19 @@ plt.ylim([0.8, 1.00])
 plt.tight_layout()
 plt.show()
 
-
-# 그리드 서치를 사용한 하이퍼파라미터 튜닝: 
+# 그리드 서치를 사용한 하이퍼파라미터 튜닝: 훈련 데이터에서 학습되지 않고 별도로 최적화되는 학습알고리즘 파라미터
 # 자동으로 복수개의 내부 모형을 생성하고 이를 모두 실행시켜서 최적 파라미터를 탐색
-
+# 리스트로 지정된 여러 하이퍼파라미터 값 전체를 모두 조사
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
 
 pipe_svc = make_pipeline(StandardScaler(), SVC(random_state=1))
-
+    # SVM을 위한 파이프라인을 훈련하고 튜닝
 param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
-
+    # 튜닝하려는 매개변수를 딕셔너리 리스트로 지정.
 param_grid = [{'svc__C': param_range, 'svc__kernel': ['linear']}, {'svc__C': param_range, 'svc__gamma': param_range, 'svc__kernel': ['rbf']}]
-
+    # 선형 SVM 은 규제 매개변수 C만 튜닝
 gs = GridSearchCV(estimator=pipe_svc, param_grid=param_grid, scoring='accuracy', cv=10, n_jobs=-1)
     # n_jobs : 내부적으로 멀티 프로세스를 사용하여 그리드서치 수행. CPU 코어의 수가 충분하다면 n_jobs를 늘릴 수록 속도가 증가. (default 1)
 gs = gs.fit(X_train, y_train)
@@ -165,15 +161,14 @@ print(gs.best_score_)
 print(gs.best_params_)
 
 
-# In[18]:
-
-
+# 독립적 테스트 세트를 이용하여 최고 모델 성능 추정:  
 clf = gs.best_estimator_
 clf.fit(X_train, y_train)
 print('테스트 정확도: %.3f' % clf.score(X_test, y_test))
 
 
-# In[19]:
+# 중첩 교차 검증을 사용한 알고리즘 선택: 바깥 k겹 검증 루프가 데이터를 train / test 로 분할. 안쪽 루프가 검증으로 모델 선택
+# 모델이 선택되면 테스트 폴드 사용하여 모델 성능 평가
 
 
 gs = GridSearchCV(estimator=pipe_svc, param_grid=param_grid, scoring='accuracy', cv=2)
@@ -330,7 +325,3 @@ np.mean(y_pred == y_bal) * 100
 
 
 # In[ ]:
-
-
-
-
